fe288cdc060848200d19a507faf20134bb4cca88
Artyom Filippenko <satou.rus@gmail.com>
2021-12-11 13:20:30 +1100

    add main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Main module of the project
+# Author: Filippenko Artyom, 2021
+# MISIS Master Degree Project
+# Created: 2021.12.11
+# Modified: 2021.12.11
+
+
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
