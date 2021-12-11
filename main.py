68101f8517892819539258a11a979bb61f318d94
Artyom Filippenko <satou.rus@gmail.com>
2021-12-11 13:29:35 +1100

    add constants.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-# Main module of the project
+# Main module of the project for vehicle signals recognition
 # Author: Filippenko Artyom, 2021
 # MISIS Master Degree Project
 # Created: 2021.12.11
 # Modified: 2021.12.11
 
 
-
 def main():
     pass
-
 
 
 if __name__ == '__main__':
---
+++ utils/constants.py
@@ -0,0 +1,5 @@
+# Project constants
+# Author: Filippenko Artyom, 2021
+# MISIS Master Degree Project
+# Created: 2021.12.11
+# Modified: 2021.12.11
