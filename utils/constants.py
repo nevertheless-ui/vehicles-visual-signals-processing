0ed16a95bc1bf864e9a696744e8daa4750e55635
Artyom Filippenko <satou.rus@gmail.com>
2021-12-14 19:27:29 +1100

    add extractor

--- utils/constants.py
+++ utils/constants.py
@@ -3,3 +3,11 @@
 # MISIS Master Degree Project
 # Created: 2021.12.11
 # Modified: 2021.12.11
+
+import os
+
+DATA_DIR_PATH = 'data\\raw_data'
+EXTRACTOR_RESOLUTION = (500, 500) # pixels
+VIDEO_CHUNK_SIZE = 2 # seconds
+SKIP_FRAME_NUM = 1
+TARGET_FPS = 30
