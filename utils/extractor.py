b7f6b30004b0f258fd45f0226e467cc967d58775
Artyom Filippenko <satou.rus@gmail.com>
2021-12-14 23:41:13 +1100

    rm unused imports

--- utils/extractor.py
+++ utils/extractor.py
@@ -6,10 +6,7 @@
 # TODO - add FPS ratio for convertion
 
 
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
-import cv2 as cv2
+import cv2
 import os
 import shutil
 import xmltodict
