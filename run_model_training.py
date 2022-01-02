2d740fcd5ccc11f20c37a434a9f83e8bf16ddead
Artyom Filippenko <satou.rus@gmail.com>
2022-01-02 19:42:15 +1100

    add base training module

---
+++ run_model_training.py
@@ -0,0 +1,12 @@
+# Main module of the project for vehicle signals recognition
+# Author: Filippenko Artyom, 2021
+# MISIS Master Degree Project
+
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
