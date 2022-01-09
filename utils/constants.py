2623535d05a6dae37a6e528a0958ddc7731aba1d
Artyom Filippenko <satou.rus@gmail.com>
2022-01-10 08:35:12 +1100

    add annotation constants

--- utils/constants.py
+++ utils/constants.py
@@ -4,10 +4,17 @@
 # Created: 2021.12.11
 # Modified: 2021.12.11
 
+
 import os
+
 
 DATA_DIR_PATH = 'data\\raw_data'
 EXTRACTOR_RESOLUTION = (500, 500) # pixels
 VIDEO_CHUNK_SIZE = 2 # seconds
 SKIP_FRAME_NUM = 1
 TARGET_FPS = 30
+SUPPORTED_VIDEO_FORMATS = ('.ts')
+
+CVAT_STARTSWITH = 'task_'
+CVAT_ENDSWITH = ('cvat for video 1.1.zip')
+git 
