e19cf17ef543fbe109313e66e51e2b43a38996fd
Artyom Filippenko <satou.rus@gmail.com>
2022-01-11 01:17:26 +1100

    fix namespaces of modules

---
+++ train_model.py
@@ -0,0 +1,13 @@
+"""
+Training module for the project of vehicle signals recognition
+Author: Filippenko Artyom, 2021-2022
+MISIS Master Degree Project
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
