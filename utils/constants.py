19220afdfabd7d869d7307288991a0f59257fa65
Artyom Filippenko <satou.rus@gmail.com>
2022-01-11 00:52:49 +1100

    add constants for logger

--- utils/constants.py
+++ utils/constants.py
@@ -7,14 +7,25 @@
 
 import os
 
+# LOGGER
+ENABLE_DEBUG_LOGGER = True
+LOGGER_FILENAME = 'debug.log'
+# exclude long attributes from logs
+SKIP_ATTRIBUTE = ('annotation_file',
+                  'annotation_meta',
+                  'annotation_tracks')
 
+
+# DATA
 DATA_DIR_PATH = 'data\\raw_data'
-EXTRACTOR_RESOLUTION = (500, 500) # pixels
+EXTRACTOR_RESOLUTION = (500, 500)     # pixels
 VIDEO_CHUNK_SIZE = 2 # seconds
 SKIP_FRAME_NUM = 1
 TARGET_FPS = 30
 SUPPORTED_VIDEO_FORMATS = ('.ts')
-OVERWRITE = False
+OVERWRITE = True                      # Output dataset directory
 
+
+# ANNOTATION
 CVAT_STARTSWITH = 'task_'
 CVAT_ENDSWITH = ('cvat for video 1.1.zip')
